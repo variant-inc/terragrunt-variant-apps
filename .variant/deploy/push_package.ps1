f7de220e4b97c385b3252b6e053097cd46bc92b9
Hitesh Patel <hpatel@hiteshp.me>
2022-01-04 17:04:14 -0500

    only push octopus package

---
+++ .variant/deploy/push_package.ps1
@@ -0,0 +1,44 @@
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+$WarningPreference = "SilentlyContinue"
+
+Trap
+{
+  Write-Error $_.InvocationInfo.ScriptName -ErrorAction Continue
+  $line = "$($_.InvocationInfo.ScriptLineNumber): $($_.InvocationInfo.Line)"
+  Write-Error $line -ErrorAction Continue
+  Write-Error $_
+}
+function CommandAliasFunction
+{
+  Write-Information ""
+  Write-Information "$args"
+  $cmd, $args = $args
+  & "$cmd" $args
+  if ($LASTEXITCODE)
+  {
+    throw "Exception Occured"
+  }
+  Write-Information ""
+}
+
+Set-Alias -Name ce -Value CommandAliasFunction -Scope script
+
+param (
+    [String] $PackageName,
+    [String] $Version,
+    [String] $BasePackagePath,
+    [String[]] $Spaces
+)
+
+ce octo pack `
+    --id="${PackageName}" `
+    --format="Zip" --version="${Version}" `
+    --basePath="$BasePackagePath" --outFolder="./packages"
+
+foreach ($Space in $Spaces) {
+    ce octo push `
+        --package="./packages/${PackageName}.${Version}.zip" `
+        --space="${Space}" `
+        --overwrite-mode=OverwriteExisting
+}
