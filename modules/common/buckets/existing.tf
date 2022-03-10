df34667be49010cc7a9b94b9a7ebf16ca848ac8b
Hitesh Patel <hpatel@hiteshp.me>
2022-03-10 10:14:45 -0500

    refactor buckets

---
+++ modules/common/buckets/existing.tf
@@ -0,0 +1,45 @@
+locals {
+  // Convert the list of inputs into map where each key is the bucket prefix
+  existing_map = { for existing in var.existing : existing.bucket_prefix => existing }
+}
+
+// Find ConfigMaps for each bucket owned by other apps
+data "kubernetes_config_map" "existing" {
+  for_each = local.existing_map
+
+  metadata {
+    name      = "${each.value.project_name}-bucket-${each.key}"
+    namespace = each.value.project_group
+  }
+}
+
+// Create a copy of the bucket configuration of another app for this app
+resource "kubernetes_config_map" "existing" {
+  for_each = data.kubernetes_config_map.existing
+
+  metadata {
+    name      = "${var.app_name}-bucketref-${each.key}"
+    namespace = var.namespace
+  }
+
+  data = each.value.data
+}
+
+// Create a read policy for the existing buckets
+data "aws_iam_policy_document" "existing" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetObjectAcl"
+    ]
+
+    resources = flatten([for label, configmap in kubernetes_config_map.existing : [
+      lookup(configmap.data, "BUCKET__${label}__arn", null),
+      "${lookup(configmap.data, "BUCKET__${label}__arn", null)}/*"
+      ]
+    ])
+  }
+}
