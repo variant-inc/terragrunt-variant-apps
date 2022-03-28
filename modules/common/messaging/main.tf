8b5f54fadaab8d165a06ab81658a092380dbd4de
Naveen <naveenp@drivevariant.com>
2022-03-28 09:57:34 -0400

    more changes

--- modules/common/messaging/main.tf
+++ modules/common/messaging/main.tf
@@ -21,6 +21,20 @@ module "sns_topic" {
   kms_key_sns_arn                  = data.aws_kms_key.sns_alias.arn
 }
 
+resource "kubernetes_config_map" "sns_topics" {
+  for_each = local.topic_map
+
+  metadata {
+    name      = "${var.app_name}-sns-topic-${each.key}"
+    namespace = var.namespace
+  }
+
+  data = {
+    "TOPIC__${each.key}__name" = module.sns_topic[each.key].sns_topic_name
+    "TOPIC__${each.key}__arn"  = module.sns_topic[each.key].sns_topic_arn
+  }
+}
+
 
 data "aws_sns_topic" "topics_to_subscribe" {
   for_each = local.topic_subscription_map
@@ -43,4 +57,24 @@ module "sqs_queue" {
   content_based_deduplication       = lookup(each.value, "content_based_deduplication", null)
   kms_key_sns_arn                   = data.aws_kms_key.sns_alias.arn
   kms_data_key_reuse_period_seconds = lookup(each.value, "kms_data_key_reuse_period_seconds", null)
+  depends_on = [module.sns_topic]
+}
+
+data "aws_sqs_queue" "queue_urls" {
+  for_each = module.sqs_queue
+  name     = each.value.sqs_queue_name
+}
+
+resource "kubernetes_config_map" "sns_sqs_subscriptions" {
+  for_each = local.topic_subscription_map
+  metadata {
+    name      = "${var.app_name}-sns-sqs-subscription-${each.key}"
+    namespace = var.namespace
+  }
+
+  data = {
+    "QUEUE__${each.key}__name" = module.sqs_queue[each.key].sqs_queue_name
+    "QUEUE__${each.key}__arn"  = module.sqs_queue[each.key].sqs_queue_arn
+    "QUEUE__${each.key}__url"  = data.aws_sqs_queue.queue_urls[each.key].url
+  }
 }
--- modules/common/messaging/outputs.tf
+++ modules/common/messaging/outputs.tf
@@ -7,3 +7,10 @@ output "sqs_queues" {
   description = "SQS Queues Output"
   value       = module.sqs_queue.*
 }
+
+output "config_maps" {
+  value = concat(
+    [for label, cm in kubernetes_config_map.sns_topics : cm.metadata[0].name],
+    [for label, cm in kubernetes_config_map.sns_sqs_subscriptions : cm.metadata[0].name],
+  )
+}
