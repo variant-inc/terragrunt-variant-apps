8bcdca9b34f1e33ddca846260eb76bd56b3de479
Ralph-Cheng <85500202+rchengvariant@users.noreply.github.com>
2022-04-01 09:50:03 -0400

    Update main.tf

--- modules/apps/cron/main.tf
+++ modules/apps/cron/main.tf
@@ -46,7 +46,7 @@ resource "helm_release" "cron" {
   count             = var.create == true ? 1 : 0
   repository        = "https://variant-inc.github.io/lazy-helm-charts/"
   chart             = "variant-cron"
-  version           = "1.2.1"
+  version           = "1.2.2"
   cleanup_on_fail   = true
   name              = var.name
   namespace         = local.namespace
