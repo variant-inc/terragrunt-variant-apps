15a67eb4832635a1d0e3ff507f71b242a0746284
Joel Weirauch <jweirauch@usxpress.com>
2022-05-12 14:29:25 -0400

    wip

--- modules/common/role_policies/outputs.tf
+++ modules/common/role_policies/outputs.tf
@@ -1,4 +1,4 @@
 output "policies" {
-  value       = aws_iam_policy_document.role_policies
+  value       = data.aws_iam_policy_document.role_policies
   description = "Custom role policies to attach"
 }
