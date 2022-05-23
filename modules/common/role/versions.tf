ea8a02bff34a4ab10d3d3c51603acc820ed83ccb
Lee Hughs <lhughs@usxpress.com>
2022-05-23 13:24:52 -0400

    Add custom in-line policies [Ralph]

---
+++ modules/common/role/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = "~> 1.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.74"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.8"
+    }
+  }
+}
