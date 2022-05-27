3b5e568d25310e2afb3b0fc35d167bc9b4fac112
Ralph Cheng <ralphc@drivevariant.com>
2022-05-27 11:50:55 -0400

    [test] updating version for beta

--- modules/apps/cron/main.tf
+++ modules/apps/cron/main.tf
@@ -40,7 +40,7 @@ resource "helm_release" "cron" {
   count             = var.create == true ? 1 : 0
   repository        = "https://variant-inc.github.io/lazy-helm-charts/"
   chart             = "variant-cron"
-  version           = "~1.2.0"
+  version           = "1.2.9-beta"
   cleanup_on_fail   = true
   name              = var.name
   namespace         = local.namespace
--- modules/apps/handler/main.tf
+++ modules/apps/handler/main.tf
@@ -40,7 +40,7 @@ resource "helm_release" "handler" {
   count             = var.create == true ? 1 : 0
   repository        = "https://variant-inc.github.io/lazy-helm-charts/"
   chart             = "variant-handler"
-  version           = "~1.1.0"
+  version           = "1.1.8-beta"
   cleanup_on_fail   = true
   name              = var.name
   namespace         = local.namespace
